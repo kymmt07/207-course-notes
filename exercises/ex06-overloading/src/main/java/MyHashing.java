1008611badd80a4a6898bf91caf5fc0b912d56fa
Kaho Yamamoto <kahoyamamoto@Kahos-MacBook-Air-2.local>
2026-09-19 21:29:51 -0400

    exercise 6 completed

--- exercises/ex06-overloading/src/main/java/MyHashing.java
+++ exercises/ex06-overloading/src/main/java/MyHashing.java
@@ -20,6 +20,7 @@ public class MyHashing {
   /** Creates a MyHashing whose seed starts at 0. */
   public MyHashing() {
     // TODO: this constructor takes no arguments; leave the seed at its default.
+    this.seed = 0;
   }
 
   /**
@@ -29,6 +30,7 @@ public class MyHashing {
    */
   public MyHashing(int seed) {
     // TODO: store the parameter in this object's seed field.
+    this.seed = seed;
   }
 
   /**
@@ -39,7 +41,9 @@ public class MyHashing {
    */
   public int hash(int value) {
     // TODO
-    return 0;
+    int oldSeed = this.seed;
+    this.seed = value;
+    return oldSeed;
   }
 
   /**
@@ -52,7 +56,9 @@ public class MyHashing {
    */
   public int hash(char value) {
     // TODO
-    return 0;
+    int oldSeed = this.seed;
+    this.seed = value;
+    return (oldSeed + value) % MODULO;
   }
 
   /**
@@ -65,6 +71,11 @@ public class MyHashing {
    */
   public static int hash(String value) {
     // TODO: String.toCharArray() may help.
-    return 0;
+    char[] stringArray = value.toCharArray();
+    int charSum = 0;
+    for (int i = 0; i < stringArray.length; i++){
+      charSum += stringArray[i];
+    }
+    return charSum;
   }
 }
